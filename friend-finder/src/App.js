989d5c5ceb45f6a4e51faed9c63d75a69db19350
schang1146 <sammychang2185@gmail.com>
2019-07-10 22:28:11 -0700

    scaffolding: imported dependencies and added a few routes to get started

--- friend-finder/src/App.js
+++ friend-finder/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+
+import { BrowserRouter as Router, Route, Navlinks } from 'react-router-dom';
+
+// import components
+// import PrivateRoute from './components/PrivateRoute';
+import Login from './components/LoginForm';
+import UserLanding from './components/UserLanding';
+import NewUserForm from './components/NewUserForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        </header>
+
+        {/* Routes */}
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={NewUserForm} />
+        <Route path='/landing' component={UserLanding} /> {/* Maybe change the url to user's username later */}
+        {/* <PrivateRoute exact path='' component={} /> */}
+
+      </div>
+    </Router>
   );
 }
 
