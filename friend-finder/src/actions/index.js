39e01b8b67d4e28844a73afe7cf9955156b81656
schang1146 <sammychang2185@gmail.com>
2019-07-11 18:56:41 -0700

    added notes to actions for later

--- friend-finder/src/actions/index.js
+++ friend-finder/src/actions/index.js
@@ -5,7 +5,7 @@ export const LOGIN_START = "LOGIN_START";
 export const login = creds => dispatch => {
     dispatch({ type: LOGIN_START });
     return axios
-        .post('', creds)
-        .then(res => {})
-        .catch(err => {});
+        .post('' /* endpoint for login */, creds)
+        .then(res => {} /* save token to localStorage from res */)
+        .catch(err => {} /* handle login error */);
 }
